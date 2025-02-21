e49c30766f3ff196f5af4488659ceec199888abf
Robert Silver <143738061+robert-710@users.noreply.github.com>
2025-02-21 15:15:38 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -4,8 +4,8 @@ import geni.rspec.pg as rspec
 # Create a Request object to start building the RSpec.
 request = portal.context.makeRequestRSpec()
 
-# Create a XenVM for the deployment
-node = request.XenVM("graphical-degree-progress-node")
+# Use a shorter name for the node
+node = request.XenVM("gdp-node")  # Shortened from graphical-degree-progress-node
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
 node.routable_control_ip = "true"
 
